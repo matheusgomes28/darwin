8b43cffc7f28e17bfd20f580fb45356d4ef6d056
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2017-11-26 12:12:19 +0000

    Utilities file for OpenCV  added. Mostly contains boiler code for opening images in the correct color space, greycale conversion, displaying in notebooks, etc...

---
+++ utilities.py
@@ -0,0 +1,16 @@
+"""
+This file will contain some utility code
+that will be useful when processing images.
+
+Most of the functions here will be used in 
+Jupyter notebook in order to load, show and 
+analyse images in the correct format 
+(Matplotlib), and convert between colour 
+spaces (E.g BGR -> RGB).
+"""
+
+import cv2, sys, os
+import numpy as np
+
+
+# Code here for loading stuff
