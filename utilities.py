3ef32c00233a3ac99f001e3671c69df256c41118
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2017-11-26 19:01:50 +0000

    Fixed a few problems with the colour conversion codes in OpenCV.

--- utilities.py
+++ utilities.py
@@ -22,7 +22,7 @@ convert_strs = {
 				 "BGR2RGB" : cv2.COLOR_BGR2RGB,
 				 "BGR2GRAY": cv2.COLOR_BGR2GRAY,
 				 "RGB2BGR" : cv2.COLOR_RGB2BGR,
-				 "RGG2GRAY": cv2.COLOR_RGB2GRAY,
+				 "RGB2GRAY": cv2.COLOR_RGB2GRAY,
 			   }
 
 def get_dir(path:str):
@@ -37,13 +37,13 @@ def get_dir(path:str):
 	# Change to proper OS directory
 	return path
 
-def convert_spaces(img:np.ndarray, key:str) -> np.ndarray:
+def convert_spaces(img:np.ndarray, spaces:str) -> np.ndarray:
 
 	# Return a new image in the converted space
-	return cv2.cvtColor(img, convert_strs[key])
+	return cv2.cvtColor(img, convert_strs[spaces])
 
 
-def read_image(path:str, key:str="COLOR_BGR2RGB") -> np.ndarray:
+def read_image(path:str, spaces:str="BGR2RGB") -> np.ndarray:
 	"""
 	This funtion will use OpenCV to load an image. 
 	Note that the default colour space is BGR. To 
@@ -51,11 +51,11 @@ def read_image(path:str, key:str="COLOR_BGR2RGB") -> np.ndarray:
 	'key' argument (see convert_strs). 
 
 	path - string representing the path of the image
-	key - string representing key in the convert_dir
+	spaces - string representing key in the convert_dir
 	"""
 
 	# Load image (BGR)
 	img = cv2.imread(get_dir(path))
 
 	# Returns the converted image 
-	return convert_spaces(img, convert_strs[key]) 
+	return convert_spaces(img, spaces) 
