ef849a0699fdb835a6aa47dd1440fd0f481f1e59
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2018-04-07 20:44:07 +0100

    This file is a simple CLI which allows us to crop the section of the images containing the eye, to diminish the effect of the black background on the images when analysing. For more info refer to notebook Dataset Viewing and Cropping.

--- dataset_cropping.py
+++ dataset_cropping.py
@@ -1,26 +1,34 @@
-""" Image Dataset Analysis
+""" Dataset Cropping/Resizing Script
 
-This script will analyse and obtain some features from the
-dataset given. Note that most of the code used here comes
-from the 'analysis.py' file. 
+This script will go throght all the images in the given 
+dataset and it will identify the bounding box in the 
+retinal images, crop the said bounding box containing
+the eye, resize if chosen and then save the images
+to the output directory.
 
 
 Usage:
-    dataset_analysis.py ROOT OUTPUT
+    dataset_cropping.py ROOT OUTPUT 
+    dataset_cropping.py ROOT OUTPUT -x WID -y HEI
 
 Arguments:
-    ROOT            The root directory of the image dataset.
-    OUTPUT          The output directory to save the analysis files.
+    ROOT                 The root directory of the image dataset.
+    OUTPUT               The output directory to save processed images.
+    WID                  The resizing width of the image.
+    HEI                  The resizing height of the image.
+
+Options:
+	-x WID --width=WID    Indicates resize image with given width.
+	-y HEI --height=HEI   Indicates resize image with given height.
 """
 
-import files # For the file management stuff
+import files, cv2 # For the file management stuff
 import os, sys # For directory changing, sys stuff
 import utilities as ut
 import analysis as an
 import numpy as np
 from colorama import Style, Fore, init # Colouring CLI stuff
-from docopt import docopt # CLI argument parser 
-from matplotlib import pyplot as plt
+from docopt import docopt # CLI argument parser
 
 # Function for the loading bar
 def update_line(text, chars=["\033[F","\r"]): 
@@ -31,9 +39,7 @@ def update_line(text, chars=["\033[F","\r"]):
 
     Only use this for CLIs as info is printed to stdout.
     """
-
-    # If the system is Windows, we need
-    # to flush the output to reset pointers.
+    
     if os.name == 'nt':
         # Print text and update cursor
         sys.stdout.write(text)
@@ -86,28 +92,48 @@ if __name__== "__main__":
 		print(Style.RESET_ALL)
 		sys.exit(0) # Use system's abort function
 
+	# Check for the resizing args
+	resize = False
+	if arguments['-x']:
+		# Flag to resize images
+		resize = True
+
+		# Get the width and height
+		width = int(arguments['WID'])
+		height = int(arguments['HEI'])
+
+	# Print the resizing settings
+	print("Resizing set to {0}{1}{2}.".format(Fore.GREEN, resize, Fore.BLUE))
+	if resize:
+		print("Width: {0}{1}px{2}".format(Fore.WHITE, width, Fore.BLUE))
+		print("Height: {0}{1}px{2}".format(Fore.WHITE, height, Fore.BLUE))
+
 	# Reset colour
 	print(Style.RESET_ALL)
 
 	images = files.get_images(root_path) # Note line below resets the blue 
 	print(Style.RESET_ALL + Fore.GREEN + "Number of images in the path: {:d}".format(len(images)))
-
-	# CALCULATE THE QUALITIES OF THE IMAGES
-	hist_size = 256
-	histograms = np.zeros((len(images), hist_size))  # Define matrix for storing histograms
-	stats = np.zeros((len(images), 2))				 # Define matrix for storing laplacian variance and ERR
-
 	
 	size = 20 # Length of the loading bar in chars
 	for i, img_path in enumerate(images):
-		image_gray = ut.read_image(img_path, "BGR2GRAY") #grayscale
-		lp = an.get_laplacian(image_gray)  # Laplacian
-		err = an.get_err(image_gray) # Err
+		image = ut.read_image(img_path, "BGR2RGB") #grayscale
 
-		# Fill the matrices
-		histograms[i,:] = an.get_histogram(image_gray).ravel()
-		stats[i,0] = np.var(lp)
-		stats[i,1] = err
+		# Do the image cropping and save to the output
+		rect = an.get_rect(image) # Ruple with bounding rect coords
+		cropped = image[rect[1]:rect[3],rect[0]:rect[2]] # Simple cropping
+
+		# This is the resizing stuff. Here I am assuming we
+		# will be decimating the image (reducing size), hence
+		# the interpolation used is the AREA relation.
+		if resize:
+			inter = cv2.INTER_AREA # OpenCV const for such interpolation
+			cropped = cv2.resize(cropped, (width, height), interpolation=inter)
+
+
+		# Saving the processed file 
+		name = files.get_filename(img_path) + ".jpg"
+		save_path = files.append_path(out_path, name)
+		ut.save_image(save_path, ut.convert_spaces(cropped, "RGB2BGR"))
 
 		# Progress bar stuff
 		perc = (i+1)/len(images)
@@ -116,32 +142,6 @@ if __name__== "__main__":
 		line += "="*bar + " "*(size-bar)
 		line += "] {:d}%".format(int(np.round(perc*100)))
 		update_line(line) # Thins func will use carriage return
+
 	print("\nFinished processing.")
-
-	# Get saving directories
-	stats_path = os.path.join(out_path, "stats.txt")
-	hist_path = os.path.join(out_path, "hist_model.txt")
-	# Calculate the mean histogram of images and save
-	# both stats file and the mean histogram
-	mean_hist = np.mean(histograms, axis=0) 
-	np.savetxt(hist_path, mean_hist)
-	np.savetxt(stats_path, stats)
-
-
-	# Plotting the distributions of the lap, err
-	# and plotting the mean histogram 
-	fig = plt.figure(figsize=(10, 7))
-	ax1 = plt.subplot(2,1,1)
-	ax1.set_title("Mean Histogram")
-	ax1.plot(mean_hist)
-
-	ax2 = plt.subplot(2,2,3)
-	ax2.set_title("Var of Lap Histogram")
-	ax2.hist(stats[:,0], bins=25)
-
-	ax3 = plt.subplot(2,2,4)
-	ax3.set_title("ERR Histogram")
-	ax3.hist(stats[:,1], bins=25)
-
-	fig.tight_layout()
-	plt.show()
+	print(Style.RESET_ALL)
