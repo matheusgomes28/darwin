6fb6b005585c0bd4d36a1b9e5d53aa01e32ec27a
George Baron <gbaron1@sheffield.ac.uk>
2018-04-30 10:27:33 +0100

    Adds more processing

--- evaluation/process.py
+++ evaluation/process.py
@@ -22,6 +22,13 @@ sys.path.append(os.path.abspath('../'))
 import files
 
 def gather_data(directory):
+    """ Gathers evaluation data from all csv files in a given
+    directory.
+
+    Arguments:
+        directory       Directory the csv files are stored in
+    """
+
     data = []
     paths = files.get_files(directory, ['csv'])
 
@@ -32,11 +39,39 @@ def gather_data(directory):
     
     return data
 
-def plot_responses(data):
-    # Gather response data
-    response_count = Counter([r for _, r, _ in data])
-    responses = [response_count['0'], response_count['1'], response_count['2']]
+def gather_responses(data):
+    """ Gathers response counts from supplied data
 
+    Arguments:
+        data        data in format [(filename, response, response_time)]
+    """
+
+    response_count = Counter([r for _, r, _ in data])
+    return [response_count['0'], response_count['1'], response_count['2']]
+
+def gather_response_times(data):
+    """ Gathers response times of each response from supplied data
+
+    Arguments:
+        data        data in format [(filename, response, response_time)]
+    """
+
+    averages = {}
+    response_times = [(r, t) for _, r, t in data]
+    for r, t in response_times:
+        if r not in averages:
+            averages[r] = t
+        else:
+            averages[r] += t
+    return averages
+    
+
+def plot_responses(responses):
+    """ Plots responses in a bar chart
+
+    Arguments:
+        responses       list containing response counts
+    """
     fig = plt.figure(figsize=(8, 5))
     x = ['Incorrect', 'Correct', 'Near/Close']
     y = responses
@@ -45,6 +80,9 @@ def plot_responses(data):
     plt.ylabel('# Responses')
     plt.title('Evaluation Responses')
     plt.show()
+
+def plot_response_times(data):
+    pass
 
 if __name__ == '__main__':
     # Change cwd to the current file's directory
@@ -73,4 +111,20 @@ if __name__ == '__main__':
     print(Style.RESET_ALL)
 
     data = gather_data(eval_path)
-    plot_responses(data)
+    responses = gather_responses(data)
+    response_times = gather_response_times(data)
+
+    total_responses = sum(responses)
+    i_responses = (responses[0] / total_responses) * 100.0
+    c_responses = (responses[1] / total_responses) * 100.0
+    n_responses = (responses[2] / total_responses) * 100.0
+    i_time = response_times['0'] if '0' in response_times else 0.0
+    c_time = response_times['1'] if '1' in response_times else 0.0
+    n_time = response_times['2'] if '2' in response_times else 0.0
+
+    print('Incorrect: {}%, Av. Response: {}s'.format(i_responses, i_time))
+    print('Correct: {}%, Av. Response: {}s'.format(c_responses, c_time))
+    print('Near/Close: {}%, Av. Response: {}s'.format(n_responses, n_time))
+
+    plot_responses(responses)
+    # plot_response_times(data)
