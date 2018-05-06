3e4007112671003d42c1841c58b3e423913a3143
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2018-05-06 12:20:54 +0100

    Added code to retrieve the positive images from the dataset with 'process.py'. Also fixed the plotting function.

--- evaluation/process.py
+++ evaluation/process.py
@@ -4,14 +4,17 @@ This script processes evaluation data (CSV) derived from the
 Evaluation GUI Tool and produces graphs
 
 Usage:
-    main.py EVALUATION_DATA
-    main.py --help 
+    process.py EVALUATION_DATA INPUT OUTPUT
+    process.py --help 
 
 Arguments:
     EVALUATION_DATA       The path to the evaluation data directory
+    INPUT                 The directory where the response images are located.
+    OUTPUT                The directory to copy images to (response extraction).
 """
 
 import sys, os, csv
+import shutil # For ccopying files
 from collections import Counter
 from docopt import docopt
 from colorama import Style, Fore, init 
@@ -29,9 +32,11 @@ def gather_data(directory):
         directory       Directory the csv files are stored in
     """
 
+    # Get all the csv files
     data = []
     paths = files.get_files(directory, ['csv'])
 
+    # Open each csv and dum all the responses to a list
     for p in paths:
         with open(p, 'r') as f:
             reader = csv.reader(f)
@@ -46,8 +51,11 @@ def gather_responses(data):
         data        data in format [(filename, response, response_time)]
     """
 
+    # Counter object (basically a dict with (key:counts))
     response_count = Counter([int(r) for _, r, _ in data])
-    return [response_count[0], response_count[1], response_count[2]]
+
+    # Returns counts of (0, 1, 2) corresponding to (invalid, correct, near)
+    return response_count[0], response_count[1], response_count[2]
 
 def gather_response_times(data):
     """ Gathers response times of each response from supplied data
@@ -56,16 +64,56 @@ def gather_response_times(data):
         data        data in format [(filename, response, response_time)]
     """
 
-    averages, divisor = {}, {}
+    # sums is the sum of response times (dict with key=response, val=sum_times)
+    # divisor is a dict counting the number of responses for a key
+    sums, divisor = {}, {}
+
+    # list with all the response times (label, times as float)
     response_times = [(r, t) for _, r, t in data]
+
+    # update the dicts as loop through the response list
     for r, t in response_times:
-        if r not in averages:
-            averages[r] = float(t)
+
+        # if respnse not in the dicts, add them
+        if r not in sums:
+            sums[r] = float(t)
             divisor[r] = 1
+
+        # if already in the dicts, simply update the values
         else:
-            averages[r] += float(t)
+            sums[r] += float(t)
             divisor[r] += 1
-    return {x : averages[x] / float(divisor[x]) for x in averages}
+
+    # return a dictionary containing {key=response, val=average times}
+    return {x : sums[x] / float(divisor[x]) for x in sums}
+
+def get_responsefiles(response, data):
+    """
+    Given a response value i (i=0,1,2), this function will
+    return a list with all the file names associated with 
+    the i response.
+
+    Args:
+        response - the reponse in the csv file.
+        data     - list containing the read csv responses [(name, response, time)]
+    Returns:
+        list of string containing file names ()
+    """
+
+    # List to hod the filenames corresponsing to the reponse
+    response_names = [] 
+
+    # Loop through the data adding the response names
+    # to our output list
+    for n, r, _ in data: 
+
+        # Check if correct response
+        if int(r)==int(response): response_names.append(files.get_filename(n)) 
+
+    # Return the list containing the filenames, note that the 'set'
+    # operator gets rid of duplications
+    return list(set(response_names))
+
     
 
 def plot_responses(responses):
@@ -75,7 +123,7 @@ def plot_responses(responses):
         responses       list containing response counts
     """
     fig = plt.figure(figsize=(8, 5))
-    x = ['Incorrect', 'Correct', 'Near/Close']
+    x = [0, 1, 2]
     y = responses
     plt.bar(x, y, 1, color='red')
     plt.xlabel('Response Types')
@@ -100,21 +148,59 @@ if __name__ == '__main__':
     arguments = docopt(__doc__, version="1.0")
 
     eval_path = files.abspath(arguments['EVALUATION_DATA'])
+    input_path = files.abspath(arguments['INPUT'])
+    output_path = files.abspath(arguments['OUTPUT'])
     print(Fore.BLUE + "Evaluation directory..... %s" % eval_path)
+    print(Fore.BLUE + "Image input directory.... %s" % input_path)
+    print(Fore.BLUE + "Image output directory... %s" % output_path)
 
-    if os.path.lexists(eval_path):
+    if os.path.lexists(eval_path) and os.path.lexists(input_path):
         pass
     else:
-        print(Fore.RED + "Path not found, evaluation directory does not exist.")
+        print(Fore.RED + "Path not found, evaluation/input directory does not exist.")
         print(Style.RESET_ALL)
         sys.exit(0) # Use system's abort function
     
     # Reset colour
     print(Style.RESET_ALL)
 
+    # Gather the data
     data = gather_data(eval_path)
     responses = gather_responses(data)
     response_times = gather_response_times(data)
+
+
+    ##########################################
+    ## Extracting the response images stuff ##
+    #########################################
+
+    # Get all the images in the dataset
+    image_paths = files.get_images(input_path)
+
+    # Get the list of JUST the file names from all the images
+    dataset_names = list(map(files.get_filename, image_paths))
+
+    # Get the response file names
+    response_names = get_responsefiles(1, data)
+    print("response is: {}".format(response_names))
+
+    # Make sure output path exists
+    if not os.path.exists(output_path):
+        os.mkdir(output_path) #mode = 777
+
+    # Loop to go trhough the response_times and copy the
+    # images to the output directory
+    for name in response_names:
+
+        # find location in the 'dataset_names'
+        loc = dataset_names.index(name)
+
+        # use shutil to copy this image to the output
+        extension = files.get_fileext(image_paths[loc])
+
+        # copying the file here
+        shutil.copy2(image_paths[loc], output_path)
+
 
     total_responses = sum(responses)
     i_responses = (responses[0] / total_responses) * 100.0
@@ -129,4 +215,4 @@ if __name__ == '__main__':
     print('Near/Close: {:.3f}%, Av. Response: {:.3f}s'.format(n_responses, n_time))
 
     plot_responses(responses)
-    # plot_response_times(data)
+    plot_response_times(data)
